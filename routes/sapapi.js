9c583a8460a99d963b6b980a774b5f32182299a1
Manviksagar <aallu.vidyasagar@gmail.com>
2020-12-22 22:04:56 +0530

    sagar

--- routes/sapapi.js
+++ routes/sapapi.js
@@ -9,5 +9,10 @@ router.get(("/"), async(req, res)=>{
     res.send("Sagar router module is working fine")
 })
 
+router.get(("/hello"), async(req, res)=>{
+
+    res.send("Sagar router module is working fine")
+})
+
 
 module.exports=router
