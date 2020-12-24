1d4e078a11d81437c4949f9d3eeb8a33efd3a9a7
Manviksagar <aallu.vidyasagar@gmail.com>
2020-12-24 23:26:57 +0530

    mysql added

--- sag.js
+++ sag.js
@@ -2,6 +2,34 @@
 
 const express = require("Express")
 const app = express()
+const mysql = require("mysql");
+
+
+
+
+//mqsql check
+const mysqll = mysql.createConnection({
+    user:"sag",
+    password:"",
+    host:"localhost",
+    database:"test",
+
+});
+
+app.get("/insert",(req, res)=>{
+
+    mysqll.query(
+        'INSERT INTO ncr (name,city) VALUES ("allu","delhi")',(err,result)=>
+        {
+            if(err){
+                console.log("Error came");
+            }
+            res.send(result);
+        }
+       
+    )
+}
+)
 
 
 app.get(("/"), (req, res)=>{
